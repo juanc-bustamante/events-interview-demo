510a9bb849bf421b681a8d44bbfe7ccab4c35811
Juan Camilo Bustamante <55560648+juanc-bustamante@users.noreply.github.com>
2025-03-21 00:49:17 -0500

    Add packages and config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: ['fakeimg.pl'],
+  },
 };
 
 export default nextConfig;
