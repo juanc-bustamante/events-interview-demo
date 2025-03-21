24256116709587e6d5a5250480060e399de73fb8
Juan Camilo Bustamante <55560648+juanc-bustamante@users.noreply.github.com>
2025-03-21 01:00:05 -0500

    Create Home components

---
+++ src/components/Home/Home.styled.tsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const EventsList = styled.div`
+  display: flex;
+  width: 100%;
+  flex-wrap: wrap;
+  gap: 20px;
+`;
+
+export const Container = styled.div`
+  max-width: 1200px;
+  margin: 0 auto;
+  padding: 1rem;
+`;
